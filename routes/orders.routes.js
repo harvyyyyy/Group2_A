8290f04e5143cba7ebc36eeefda44602b003bd20
Christian Candia <christiancandia39@gmail.com>
2026-09-22 15:58:57 +0800

    feat(orders.routes.js): Added post method

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -6,8 +6,16 @@ const orders = [
   { id: 2, uname: 'Kim' },
 ];
 
-app.get('/users', (_req, res) => {
+router.get('/orders', (_req, res) => {
   res.json(orders);
+});
+
+router.post('/orders', (req, res) => {
+  const newUser = {
+    id: Date.now(),
+    name: req.body.name,
+  };
+  res.status(201).json(newUser);
 });
 
 module.exports = router
